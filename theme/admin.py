0c0883fe8b333bcbeb457496c122e9079fb3b6d3
Wakabayashi Yuya <wakabayashi.yuya.72n@st.kyoto-u.ac.jp>
2020-06-12 21:46:35 +0900

    add: Theme model/admin
    Theme 関連の model を作成し、admin にも登録

--- theme/admin.py
+++ theme/admin.py
@@ -1,3 +1,119 @@
 from django.contrib import admin
 
-# Register your models here.
+from theme import models
+
+
+@admin.register(models.Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    """ [Admin] 統一テーマ案
+    """
+    fieldsets = (
+        ('応募内容', {'fields': ('theme', 'description')}),
+        ('応募者情報', {'fields': (
+            'writer',
+        )}),
+        ('状態', {'fields': (
+            'create_datetime', 'accepted', 'final'
+        )}),
+    )
+
+    list_display = (
+        'theme', 'writer', 'create_datetime', 'accepted', 'final'
+    )
+
+    search_fields = (
+        'theme',
+    )
+
+    autocomplete_fields = (
+        'writer',
+    )
+
+    list_filter = (
+        'accepted', 'final'
+    )
+
+    readonly_fields = (
+        'create_datetime',
+    )
+
+
+@admin.register(models.FirstVote)
+class FirstVoteAdmin(admin.ModelAdmin):
+    """ [Admin] 予選投票
+    """
+    fieldsets = (
+        (None, {'fields': ('theme', 'eptid', 'vote_datetime')}),
+    )
+
+    list_display = (
+        'theme', 'vote_datetime'
+    )
+
+    autocomplete_fields = (
+        'theme',
+    )
+
+    readonly_fields = (
+        'vote_datetime',
+    )
+
+
+@admin.register(models.FinalVote)
+class FinalVoteAdmin(admin.ModelAdmin):
+    """ [Admin] 決戦投票
+    """
+    fieldsets = (
+        (None, {'fields': ('theme', 'eptid', 'vote_datetime')}),
+    )
+
+    list_display = (
+        'theme', 'vote_datetime'
+    )
+
+    autocomplete_fields = (
+        'theme',
+    )
+
+    readonly_fields = (
+        'vote_datetime',
+    )
+
+
+@admin.register(models.PeriodApplication)
+class PeriodApplicationAdmin(admin.ModelAdmin):
+    """ [Admin] テーマ案募集期間
+    """
+    fieldsets = (
+        (None, {'fields': ('start', 'finish')}),
+    )
+
+    list_display = (
+        'start', 'finish'
+    )
+
+
+@admin.register(models.PeriodFirstVote)
+class PeriodFirstVoteAdmin(admin.ModelAdmin):
+    """ [Admin] 予選投票期間
+    """
+    fieldsets = (
+        (None, {'fields': ('start', 'finish')}),
+    )
+
+    list_display = (
+        'start', 'finish'
+    )
+
+
+@admin.register(models.PeriodFinalVote)
+class PeriodFinalVoteAdmin(admin.ModelAdmin):
+    """ [Admin] 決選投票期間
+    """
+    fieldsets = (
+        (None, {'fields': ('start', 'finish')}),
+    )
+
+    list_display = (
+        'start', 'finish'
+    )
--- theme/models.py
+++ theme/models.py
@@ -1,3 +1,161 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Theme(models.Model):
+    # settings
+    class Meta:
+        verbose_name = '統一テーマ案'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.theme
+
+    # fields
+    theme = models.CharField(
+        verbose_name='統一テーマ',
+        max_length=100
+    )
+
+    description = models.CharField(
+        verbose_name='趣意文',
+        max_length=400,
+        validators=[MinLengthValidator(100)]
+    )
+
+    writer = models.OneToOneField(
+        'home.User',
+        verbose_name='応募者',
+        on_delete=models.CASCADE
+    )
+
+    create_datetime = models.DateTimeField(
+        verbose_name='応募日時',
+        auto_now_add=True
+    )
+
+    accepted = models.BooleanField(
+        verbose_name='受理',
+        default=False
+    )
+
+    final = models.BooleanField(
+        verbose_name='決勝進出',
+        default=False
+    )
+
+
+class FirstVote(models.Model):
+    # settings
+    class Meta:
+        verbose_name = '予選投票'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return str(self.theme)
+
+    # fields
+    theme = models.ForeignKey(
+        'theme.Theme',
+        verbose_name='投票',
+        on_delete=models.CASCADE
+    )
+
+    eptid = models.CharField(
+        verbose_name='投票者 eptid',
+        max_length=400,
+        unique=True
+    )
+
+    vote_datetime = models.DateTimeField(
+        verbose_name='投票日時',
+        auto_now_add=True
+    )
+
+
+class FinalVote(models.Model):
+    # settings
+    class Meta:
+        verbose_name = '決戦投票'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return str(self.theme)
+
+    # fields
+    theme = models.ForeignKey(
+        'theme.Theme',
+        verbose_name='投票',
+        on_delete=models.CASCADE
+    )
+
+    eptid = models.CharField(
+        verbose_name='投票者 eptid',
+        max_length=400,
+        unique=True
+    )
+
+    vote_datetime = models.DateTimeField(
+        verbose_name='投票日時',
+        auto_now_add=True
+    )
+
+
+class PeriodApplication(models.Model):
+    # settings
+    class Meta:
+        verbose_name = 'テーマ案募集期間'
+        verbose_name_plural = verbose_name
+        ordering = ('start',)
+
+    def __str__(self):
+        return '-'.join([str(self.start), str(self.finish)])
+
+    # fields
+    start = models.DateTimeField(
+        verbose_name='開始'
+    )
+
+    finish = models.DateTimeField(
+        verbose_name='終了'
+    )
+
+
+class PeriodFirstVote(models.Model):
+    # settings
+    class Meta:
+        verbose_name = '予選投票期間'
+        verbose_name_plural = verbose_name
+        ordering = ('start',)
+
+    def __str__(self):
+        return '-'.join([str(self.start), str(self.finish)])
+
+    # fields
+    start = models.DateTimeField(
+        verbose_name='開始'
+    )
+
+    finish = models.DateTimeField(
+        verbose_name='終了'
+    )
+
+
+class PeriodFinalVote(models.Model):
+    # settings
+    class Meta:
+        verbose_name = '決選投票期間'
+        verbose_name_plural = verbose_name
+        ordering = ('start',)
+
+    def __str__(self):
+        return '-'.join([str(self.start), str(self.finish)])
+
+    # fields
+    start = models.DateTimeField(
+        verbose_name='開始'
+    )
+
+    finish = models.DateTimeField(
+        verbose_name='終了'
+    )
